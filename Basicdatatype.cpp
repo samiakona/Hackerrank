5fdd27c4f6cf5924db27b483b6a738b0ea363e81
Samia_Kona <69978344+samiakona@users.noreply.github.com>
2021-08-22 22:48:29 +0600

    Add files via upload

---
+++ Basicdatatype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    long long int l;
+    char k;
+    float f;
+    double d;
+    cin >> i >> l >> k >> f >> d;
+    cout << i << endl;
+    cout << l << endl;
+    cout << k << endl;
+    printf("%.2f\n", f);
+    printf("%.5lf", d);
+
+    return 0;
+}
